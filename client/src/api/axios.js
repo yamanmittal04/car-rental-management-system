877f78a3628909fc74d3752cf70ab789f6db6ef2
Yaman Mittal <yamanmittal11@gmail.com>
2026-03-22 22:32:36 +0530

    Update axios config

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,18 @@
 import axios from "axios";
-const instance = axios.create({ baseURL: "https://car-rental-management-system-4pby.onrender.com/api" });
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
+
 export default instance;
