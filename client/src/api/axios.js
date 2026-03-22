1f4a449af969cdfc7bb53123dabb514a384c9d57
Yaman Mittal <yamanmittal11@gmail.com>
2026-03-22 21:18:08 +0530

    Updated API URL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const instance = axios.create({ baseURL: "http://localhost:5000/api" });
+const instance = axios.create({ baseURL: "https://car-rental-management-system-4pby.onrender.com/api" });
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
